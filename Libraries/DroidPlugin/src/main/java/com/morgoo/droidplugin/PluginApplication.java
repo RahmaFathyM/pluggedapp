2657b04ef2c74dc5b975b91a7026fd7e329fbe30
RahmaFathyM <rahmafatjy65@gmail.com>
2022-08-28 15:20:55 +0200

    Initial commit

---
+++ Libraries/DroidPlugin/src/main/java/com/morgoo/droidplugin/PluginApplication.java
@@ -0,0 +1,59 @@
+/*
+**        DroidPlugin Project
+**
+** Copyright(c) 2015 Andy Zhang <zhangyong232@gmail.com>
+**
+** This file is part of DroidPlugin.
+**
+** DroidPlugin is free software: you can redistribute it and/or
+** modify it under the terms of the GNU Lesser General Public
+** License as published by the Free Software Foundation, either
+** version 3 of the License, or (at your option) any later version.
+**
+** DroidPlugin is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+** Lesser General Public License for more details.
+**
+** You should have received a copy of the GNU Lesser General Public
+** License along with DroidPlugin.  If not, see <http://www.gnu.org/licenses/lgpl.txt>
+**
+**/
+
+package com.morgoo.droidplugin;
+
+import android.app.Application;
+import android.content.Context;
+import android.os.RemoteException;
+import android.widget.Toast;
+
+import com.morgoo.droidplugin.pm.PluginManager;
+
+/**
+ * Created by Andy Zhang(zhangyong232@gmail.com) 2014/12/5.
+ */
+public class PluginApplication extends Application {
+
+    private static final String TAG = PluginApplication.class.getSimpleName();
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        PluginHelper.getInstance().applicationOnCreate(getBaseContext());
+        try {
+
+
+            PluginManager.getInstance().installPackage("C:\\Users\\hp\\AndroidStudioProjects\\Embed_unityLibrary_task\\app\\build\\intermediates\\apk\\debug",0);
+        } catch (RemoteException e) {
+
+            e.printStackTrace();
+        }
+    }
+
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        PluginHelper.getInstance().applicationAttachBaseContext(base);
+        super.attachBaseContext(base);
+    }
+}
---
+++ app/src/main/java/com/example/pluggedapp/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.pluggedapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.RemoteException;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.morgoo.droidplugin.PluginApplication;
+import com.morgoo.droidplugin.PluginHelper;
+import com.morgoo.droidplugin.pm.PluginManager;
+
+public class MainActivity extends AppCompatActivity {
+Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn=findViewById(R.id.btn);
+
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"kkk",Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(MainActivity.this, PluginApplication.class));
+            }
+        });
+}
+
+
+}
